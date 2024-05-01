fb8840b18698d623d0b0d49fe8865c7f2a7ed340
Rodrigo Amigo Morgado <amigo.rodrigo.morgado@gmail.com>
2024-05-01 11:01:41 -0400

    adicion de consumo API , e informacion basica de 1 pokemon

--- JS/index.js
+++ JS/index.js
@@ -1,31 +1,57 @@
-$.ajax({
-    url: 'https://dragonball-api.com/api/characters/',
-    type: 'GET',
-    success: function (data) {
-        console.log(data);
-    },
-    error: function (error) {
-        console.log('Error: ', error);
-    }
-});
-
-$.ajax({
-    type: "method",
-    url: "url",    
-    success: function (response) {
-        
-    }
-});
-
-// fetch('https://pokeapi.co/api/v2/', {
-//     method: 'GET',
+// traer datos pokeapi
+// JavaSript (metodo fetch)
+// fetch("https://pokeapi.co/api/v2/", {
+//   method: "GET",
 // })
-// .then(response => {
+//   .then((response) => {
 //     if (response.ok) {
-//         return response.json();
+//       return response.json();
 //     } else {
-//         throw new Error('Network response was not ok.');
+//       throw new Error("Network response was not ok.");
 //     }
-// })
-// .then(data => console.log(data))
-// .catch(error => console.error('Error:', error));
+//   })
+//   .then((data) => console.log(data))
+//   .catch((error) => console.error("Error:", error));
+
+// Forma con JQuery
+$.ajax({
+  type: "GET", //GET POST PUT PATCH DELETE ETC.
+  url: "https://pokeapi.co/api/v2/pokemon/1/", // URL A LA API A CONSUMIR
+  success: function (response) {
+    let pkmnSeleccionado = response
+    // Agregar imagen de bulbasaur de manera dinámica
+    $('#idImgPkmn').attr('src', pkmnSeleccionado.sprites.front_default);
+    $('#idImgPkmn').attr('alt', pkmnSeleccionado.name);
+    // Agregar información básica de bulbasaur
+    // 1) Tipo de pkmn
+    // 2) Peso
+    // 3) Habilidades
+
+    // Primero obtener tipo de pkmn
+    // Si yo tengo un elemento html al cual quiero rellenar con info usamos prepend y append
+    $('#idTituloPkmn').append(`${pkmnSeleccionado.name.toUpperCase()}`)
+
+    //Aqui nosotros reescribimos el html
+    // Agregar tipo pkmn
+    // Agregar peso
+    $('#idPkmnInfo').html(`
+        <p class='lead'>PKMN Type : </p>
+        <ul class='lead'>
+            <li>${pkmnSeleccionado.types[0].type.name.toUpperCase()}</li>
+            <li>${pkmnSeleccionado.types[1].type.name.toUpperCase()}</li>
+        </ul>
+        <p class='lead'>PKMN Weight : ${pkmnSeleccionado.weight} </p>
+
+        <label>PKMN Cry : </label>
+        <audio controls>
+            <source src="" type="audio/ogg" id="idAudioPkmn">
+        </audio>
+    `);
+
+    $('#idAudioPkmn').attr('src', pkmnSeleccionado.cries.legacy);
+    
+  },
+  error: function (error) {
+    console.log(error);
+  },
+});
